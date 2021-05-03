5574824441ac514ce16b7fc6cf4b05d081e744ba
Vivian Dang <47481915+viv-ishere@users.noreply.github.com>
2021-05-03 15:54:33 -0400

    stuff

--- src/UserInterface/CreateAccountPage.java
+++ src/UserInterface/CreateAccountPage.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
@@ -29,6 +30,7 @@ public class CreateAccountPage {
 		Scene scene = new Scene(group, 700, 500);
 		stage.setResizable(false);
 
+		scene.setFill(Color.LIGHTGREEN);
 		stage.setTitle("Create Account");
 		stage.setScene(scene);
 		stage.show();
