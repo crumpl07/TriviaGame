6efa3804e80e5a100d29349670554523620a5491
Vivian Dang <47481915+viv-ishere@users.noreply.github.com>
2021-05-03 20:59:00 -0400

    Update TriviaSelection.java

--- src/UserInterface/TriviaSelection.java
+++ src/UserInterface/TriviaSelection.java
@@ -3,8 +3,12 @@ package UserInterface;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TriviaSelection 
@@ -15,9 +19,12 @@ public class TriviaSelection
 		
 		backButton(group, stage);
 		
+		Button quiz = new Quiz("poodles");
+		group.getChildren().add(quiz);
+		
 		Scene scene = new Scene(group, 700, 500);
 		stage.setResizable(false);
-
+		scene.setFill(Color.BLANCHEDALMOND);
 		stage.setTitle("Select Trivia");
 		stage.setScene(scene);
 		stage.show();
@@ -37,4 +44,44 @@ public class TriviaSelection
 		
 		group.getChildren().add(button);
 	}
+	
+	public class Quiz extends Button
+	{
+		
+		public Quiz(String title)
+		{
+			super(title);
+			this.setStyle("-fx-background-color: lightblue");
+			this.setPrefSize(400, 65);
+			this.setFont(Font.font("Comic Sans MS", FontWeight.BOLD, 30));
+			this.relocate(10, 10);
+			this.setOnAction(e->{
+				
+			});
+			
+			this.setOnMouseEntered(e->{
+				this.setStyle("-fx-background-color: skyblue");
+			});
+			
+			this.setOnMouseExited(e->{
+				this.setStyle("-fx-background-color: lightblue");
+			});
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
